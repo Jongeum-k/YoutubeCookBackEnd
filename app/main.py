08bb49bb3482254b5a7b0a57e384d221c7997e4a
Jongeum-k <k.jongeum@gmail.com>
2026-08-26 18:57:33 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -39,6 +39,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "https://jongeum-k.github.io"
     ],
     allow_credentials=True,
     allow_methods=["GET"],
